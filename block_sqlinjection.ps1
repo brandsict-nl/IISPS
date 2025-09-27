2fd58fc893ba4cf7bb85f159a715381bf5e2d654
brandsict-nl <wiebren@brandsict.nl>
2025-09-27 11:34:31 +0200

    Add files via upload

--- block_sqlinjection.ps1
+++ block_sqlinjection.ps1
@@ -1,4 +1,3 @@
-# https://github.com/brandsict-nl/IISPS
 Import-Module WebAdministration
 
 function createFilterRule {
@@ -21,33 +20,38 @@ function createFilterRule {
 	
 }
 
-# sql injection string cast(
+# 1. Function-Based SQL Injection Rules
 createFilterRule -ruleName "SQL-BLOCK-seq-cast" -denyString "cast("
-createFilterRule -ruleName "SQL-BLOCK-at-version" -denyString "@@version"
-createFilterRule -ruleName "SQL-BLOCK-connect" -denyString "Connect()"
-createFilterRule -ruleName "SQL-BLOCK-cast" -denyString "cast()"
 createFilterRule -ruleName "SQL-BLOCK-char" -denyString "char()"
 createFilterRule -ruleName "SQL-BLOCK-bchar" -denyString "bchar()"
-createFilterRule -ruleName "SQL-BLOCK-sysdatabases" -denyString "sysdatabases"
-createFilterRule -ruleName "SQL-BLOCK-Select" -denyString "(select"
 createFilterRule -ruleName "SQL-BLOCK-convert" -denyString "convert("
-createFilterRule -ruleName "SQL-BLOCK-DBNETLIB" -denyString "DBNETLIB"
-createFilterRule -ruleName "SQL-BLOCK-connect2" -denyString "Connect("
-createFilterRule -ruleName "SQL-BLOCK-int" -denyString "int%2c("
-createFilterRule -ruleName "SQL-BLOCK-sysobject" -denyString "sysobject"
 createFilterRule -ruleName "SQL-BLOCK-count" -denyString "count("
 
-createFilterRule -ruleName "SQL-BLOCK-xp_cmdshell" -denyString "xp_cmdshell"
-createFilterRule -ruleName "SQL-BLOCK-sp_executesql" -denyString "sp_executesql"
-createFilterRule -ruleName "SQL-BLOCK-exec" -denyString "exec("
-createFilterRule -ruleName "SQL-BLOCK-union-select" -denyString "union+select"
-createFilterRule -ruleName "SQL-BLOCK-info-schema" -denyString "information_schema"
+# 2. System and Database Metadata Access Rules
+createFilterRule -ruleName "SQL-BLOCK-at-version" -denyString "@@version"
+createFilterRule -ruleName "SQL-BLOCK-sysobject" -denyString "sysobject"
+createFilterRule -ruleName "SQL-BLOCK-sysdatabases" -denyString "sysdatabases"
 createFilterRule -ruleName "SQL-BLOCK-syscolumns" -denyString "syscolumns"
+createFilterRule -ruleName "SQL-BLOCK-info-schema" -denyString "information_schema"
 createFilterRule -ruleName "SQL-BLOCK-master-dbo" -denyString "master.dbo"
-createFilterRule -ruleName "SQL-BLOCK-drop-table" -denyString "drop+table"
-createFilterRule -ruleName "SQL-BLOCK-truncate-table" -denyString "truncate+table"
+
+# 3. Connection and Execution Rules
+createFilterRule -ruleName "SQL-BLOCK-connect" -denyString "Connect("
+createFilterRule -ruleName "SQL-BLOCK-DBNETLIB" -denyString "DBNETLIB"
+createFilterRule -ruleName "SQL-BLOCK-exec" -denyString "exec("
+createFilterRule -ruleName "SQL-BLOCK-sp_executesql" -denyString "sp_executesql"
+createFilterRule -ruleName "SQL-BLOCK-xp_cmdshell" -denyString "xp_cmdshell"
+
+# 4. Query Manipulation Rules
+createFilterRule -ruleName "SQL-BLOCK-Select" -denyString "(select"
+createFilterRule -ruleName "SQL-BLOCK-union-select" -denyString "union+select"
+createFilterRule -ruleName "SQL-BLOCK-one-equals-one" -denyString "1=1"
+createFilterRule -ruleName "SQL-BLOCK-encoded-one-equals-one" -denyString "1%3d1"
+
+# 5. Data Type and Structure Rules
+createFilterRule -ruleName "SQL-BLOCK-int" -denyString "int%2c("
 createFilterRule -ruleName "SQL-BLOCK-nvarchar" -denyString "nvarchar"
 
-createFilterRule -ruleName "SQL-BLOCK-one-equals-one" -denyString "1=1"
-
-createFilterRule -ruleName "SQL-BLOCK-encoded-one-equals-one" -denyString "1%3d1"
+# 6. Destructive Action Rules
+createFilterRule -ruleName "SQL-BLOCK-drop-table" -denyString "drop+table"
+createFilterRule -ruleName "SQL-BLOCK-truncate-table" -denyString "truncate+table"
